19e5d37c030bda73293dff9543f6e6083ac2edc9
Rainer Sawitzki <trainng@rainer-sawitzki.de>
2019-01-08 10:14:18 +0100

    add curl

--- webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/StoreWebService.java
+++ webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/StoreWebService.java
@@ -12,9 +12,13 @@ public class StoreWebService {
 
     @Autowired private StoreService storeService;
 
-    @GetMapping(path="/store/{cat}/{id}") public int retrieveStock(@PathVariable("cat") String category, @PathVariable("id") String id){
+    @GetMapping(path="/store/{cat}/{id}", produces = "text/plain") public int retrieveStock(@PathVariable("cat") String category, @PathVariable("id") String id){
         System.out.println("retrieving stock for cat=" + category + ", id=" + id + " from path");
         return storeService.getStock(category, id);
+    }
+    @GetMapping(path="/store/{cat}/{id}", produces = "text/html") public String retrieveStock2(@PathVariable("cat") String category, @PathVariable("id") String id){
+        System.out.println("retrieving stock for cat=" + category + ", id=" + id + " from path and produces text/html");
+        return "" + storeService.getStock(category, id);
     }
 
     @GetMapping(path="/store") public int retrieveStockWithQueryParams(@RequestParam("cat") String category, @RequestParam("id") String id){
