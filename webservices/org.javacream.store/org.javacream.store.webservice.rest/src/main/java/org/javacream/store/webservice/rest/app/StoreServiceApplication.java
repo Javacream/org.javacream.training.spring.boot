736a4ff180094eb6ad439a3c89b97f0e541f1a05
Rainer Sawitzki <trainng@rainer-sawitzki.de>
2019-01-08 15:12:51 +0100

    add swagger

--- webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/app/StoreServiceApplication.java
+++ webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/app/StoreServiceApplication.java
@@ -2,11 +2,28 @@ package org.javacream.store.webservice.rest.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "org.javacream.store")
+@EnableSwagger2
 public class StoreServiceApplication {
 
     public static void main(String[] args){
         SpringApplication.run(StoreServiceApplication.class);
+    }
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.javacream.store.webservice.rest"))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
