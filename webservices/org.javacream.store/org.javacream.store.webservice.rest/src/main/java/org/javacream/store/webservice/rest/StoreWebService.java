208d816d7395991bc802f519c3a654c01092eacb
Rainer Sawitzki <trainng@rainer-sawitzki.de>
2019-01-09 10:29:27 +0100

    add swagger

--- webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/StoreWebService.java
+++ webservices/org.javacream.store/org.javacream.store.webservice.rest/src/main/java/org/javacream/store/webservice/rest/StoreWebService.java
@@ -14,16 +14,6 @@ public class StoreWebService {
         System.out.println("retrieving stock for cat=" + category + ", id=" + id + " from path");
         return new StoreResult(storeService.getStock(category, id));
     }
-
-    @GetMapping(path="/store") public int retrieveStockWithQueryParams(@RequestParam("cat") String category, @RequestParam("id") String id){
-        System.out.println("retrieving stock for cat=" + category + ", id=" + id + " from query");
-        return storeService.getStock(category, id);
-    }
-
-    @PostMapping(path="/store/{cat}/{id}/{stock}") public void setStock(@PathVariable("cat") String category, @PathVariable("id") String id, @PathVariable("stock") int stock){
-        System.out.println("setting stock for cat=" + category + ", id=" + id + " with stock=" + stock);
-        storeService.setStock(category, id, stock);
-    }
     @PostMapping(path="/store", consumes = MediaType.APPLICATION_JSON_VALUE) public void setStockWithEntry(@RequestBody StoreInfo info){
         System.out.println("setting stock for cat=" + info.getCategory() + ", id=" + info.getId() + " with stock=" + info.getStock());
         storeService.setStock(info.getCategory(), info.getId(), info.getStock());
